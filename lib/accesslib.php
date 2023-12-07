af64ffb93abb20cf6a277673abd7ae041e91b800
Safat <safat.shahin@moodle.com>
2023-12-08 09:22:31 +1100

    fixup! MDL-78551 core: Core api updates for comm api refactor

--- lib/accesslib.php
+++ lib/accesslib.php
@@ -1645,17 +1645,12 @@ function role_assign($roleid, $userid, $contextid, $component = '', $itemid = 0,
 
     core_course_category::role_assignment_changed($roleid, $context);
 
-    // Update the room membership and power levels when the user role changes.
-    if (\core_communication\api::is_available() && $coursecontext = $context->get_course_context(false)) {
-        $communication = \core_communication\api::load_by_instance(
-            $coursecontext,
-            'core_course',
-            'coursecommunication',
-            $coursecontext->instanceid,
-        );
-
-        $communication->update_room_membership([$userid]);
-    }
+    // Update the room membership when the user role changes.
+    core_course\communication\communication_helper::update_communication_room_membership(
+        course: get_course($context->get_course_context(false)->instanceid),
+        userids: [$userid],
+        memberaction: 'update_room_membership',
+    );
 
     $event = \core\event\role_assigned::create(array(
         'context' => $context,
@@ -1762,20 +1757,12 @@ function role_unassign_all(array $params, $subcontexts = false, $includemanual =
             $event->add_record_snapshot('role_assignments', $ra);
             $event->trigger();
             core_course_category::role_assignment_changed($ra->roleid, $context);
-
-            // Update the room membership and power levels when the user role changes.
-            if (\core_communication\api::is_available() && $coursecontext = $context->get_course_context(false)) {
-                $communication = \core_communication\api::load_by_instance(
-                    $coursecontext,
-                    'core_course',
-                    'coursecommunication',
-                    $coursecontext->instanceid,
-                );
-
-                $communication->update_room_membership([$ra->userid]);
-            }
-
-
+            // Update the room membership when the user role changes.
+            core_course\communication\communication_helper::update_communication_room_membership(
+                course: get_course($context->get_course_context(false)->instanceid),
+                userids: [$ra->userid],
+                memberaction: 'update_room_membership',
+            );
         }
     }
     unset($ras);
