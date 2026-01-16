56fc8243bcd25a467a4e9975259cea5cd1b53923
Conn Warwicker <conn.warwicker@catalyst-eu.net>
2026-01-16 14:45:00 +0000

    MDL-86006 mod_assign: Add multimarking database tables and fields
    Add new database tables and fields, and remove references to old
    "allocatedmarker" user flag field.

--- public/mod/assign/db/upgrade.php
+++ public/mod/assign/db/upgrade.php
@@ -131,5 +131,80 @@ function xmldb_assign_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2026022300, 'assign');
     }
 
+    if ($oldversion < 2026031000) {
+
+        // Define field markercount to be added to assign.
+        $table = new xmldb_table('assign');
+        $field = new xmldb_field('markercount', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '1', 'gradepenalty');
+
+        // Conditionally launch add field markercount.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Define field multimarkmethod to be added to assign.
+        $field = new xmldb_field('multimarkmethod', XMLDB_TYPE_CHAR, '10', null, null, null, null, 'markercount');
+
+        // Conditionally launch add field multimarkmethod.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Define field multimarkrounding to be added to assign.
+        $field = new xmldb_field('multimarkrounding', XMLDB_TYPE_INTEGER, '2', null, null, null, null, 'multimarkmethod');
+
+        // Conditionally launch add field multimarkrounding.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Define table assign_mark to be created.
+        $table = new xmldb_table('assign_mark');
+
+        // Adding fields to table assign_mark.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('assignment', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('gradeid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('marker', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('mark', XMLDB_TYPE_NUMBER, '10, 5', null, null, null, null);
+        $table->add_field('workflowstate', XMLDB_TYPE_CHAR, '20', null, null, null, null);
+
+        // Adding keys to table assign_mark.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('assignment', XMLDB_KEY_FOREIGN, ['assignment'], 'assign', ['id']);
+        $table->add_key('gradeid', XMLDB_KEY_FOREIGN, ['gradeid'], 'assign_grades', ['id']);
+        $table->add_key('marker', XMLDB_KEY_FOREIGN, ['marker'], 'user', ['id']);
+
+        // Conditionally launch create table for assign_mark.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Define table assign_allocated_marker to be created.
+        $table = new xmldb_table('assign_allocated_marker');
+
+        // Adding fields to table assign_allocated_marker.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('student', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('assignment', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+        $table->add_field('marker', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+
+        // Adding keys to table assign_allocated_marker.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+        $table->add_key('student', XMLDB_KEY_FOREIGN, ['student'], 'user', ['id']);
+        $table->add_key('assignment', XMLDB_KEY_FOREIGN, ['assignment'], 'assign', ['id']);
+        $table->add_key('marker', XMLDB_KEY_FOREIGN, ['marker'], 'user', ['id']);
+
+        // Conditionally launch create table for assign_allocated_marker.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Assign savepoint reached.
+        upgrade_mod_savepoint(true, 2026031000, 'assign');
+    }
+
     return true;
 }
